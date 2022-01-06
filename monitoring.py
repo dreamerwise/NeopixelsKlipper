a697270687801f5880c21b1cec1b2a7e1e63d2d2
dreamerwise <dreamerwise@gmail.com>
2022-01-06 21:24:12 +0100

    Logging extended

--- monitoring.py
+++ monitoring.py
@@ -21,26 +21,41 @@ class StatusMonitor:
         config.read(configFile)
         try:
             self.bed_min = config.getint('Bed', 'min_temp')
+            logging.info("Minimum bed temperature set to {}".format(self.bed_min))
             self.bed_max = config.getint('Bed', 'max_temp')
+            logging.info("Maximum bed temperature set to {}".format(self.bed_max))
             self.bed_color = tuple(map(int, (config.get('Bed', 'main_color')).split(',')))
+            logging.info("Bed main color set to {}".format(self.bed_color))
             self.bed_heating_color = tuple(map(int, (config.get('Bed', 'heating_color')).split(',')))
+            logging.info("Bed heating color set to {}".format(self.bed_heating_color))
             self.bed_cooling_color = tuple(map(int, (config.get('Bed', 'cooling_color')).split(',')))
+            logging.info("Bed cooling color set to {}".format(self.bed_cooling_color))
             self.extruder_min = config.getint('Extruder', 'min_temp')
+            logging.info("Minimum extruder temperature set to {}".format(self.extruder_min))
             self.extruder_max = config.getint('Extruder', 'max_temp')
+            logging.info("Maximum extruder temperature set to {}".format(self.extruder_max))
             self.extruder_color = tuple(map(int, (config.get('Extruder', 'main_color')).split(',')))
+            logging.info("Extruder main color set to {}".format(self.extruder_color))
             self.extruder_heating_color = tuple(map(int, (config.get('Extruder', 'heating_color')).split(',')))
+            logging.info("Extruder heating color set to {}".format(self.extruder_heating_color))
             self.extruder_cooling_color = tuple(map(int, (config.get('Extruder', 'cooling_color')).split(',')))
+            logging.info("Extruder cooling color set to {}".format(self.extruder_cooling_color))
             self.offsets = [config.getint('Rings', 'first_offset'),
                             config.getint('Rings', 'second_offset'),
                             config.getint('Rings', 'third_offset')]
+            logging.info("Ring offsets set to {}".format(self.offsets))
             self.time_interval = config.getfloat('Animation', 'time_interval')
+            logging.info("Animation time interval set to {}".format(self.time_interval))
             self.ring_order = list(map(int, (config.get('Rings', 'order').split(','))))
+            logging.info("Rings order set to {}".format(self.ring_order))
             pin_selector = [board.D10, board.D12, board.D18, board.D21]
             pin_enumerator = ["10", "12", "18", "21"]
             self.pixel_pin = pin_selector[pin_enumerator.index(config.get('GPIO', 'communincation_pin'))]
+            logging.info("Communication GPIO pin set to {}".format(self.pixel_pin))
             self.power_monitor = config.getboolean('Power', 'power_monitor')
             if self.power_monitor:
                 logging.info("Using power monitor")
+            logging.info("Whole config loaded")
 
         except:
             logging.error("missing configuration")
@@ -67,6 +82,12 @@ class StatusMonitor:
             None: None
         }
 
+        self.power_loger_svitch = True
+        self.printer_loger_svitch = True
+        self.job_loger_svitch = True
+        self.object_loger_svitch = True
+        self.led_loger_svitch = True
+
         self.pixels = neopixel.NeoPixel(
             self.pixel_pin, self.num_pixels, brightness=0.2, auto_write=False,
             pixel_order=neopixel.GRB
@@ -76,7 +97,7 @@ class StatusMonitor:
             self.pixels[i] = (0, 0, 0)
         self.pixels.show()
 
-        self.t = multiprocessing.Process(target=waiting, args=(self.time_interval, self.pixels))
+        self.start_animation = multiprocessing.Process(target=waiting, args=(self.time_interval, self.pixels))
 
     def check_status(self):
 
@@ -85,9 +106,13 @@ class StatusMonitor:
                 power = requests.get("http://localhost:7125/machine/device_power/device?device=printer")
                 power_dict = power.json()
                 self.power_status = str(power_dict['result']['printer'])
-
+                if not self.power_loger_svitch:
+                    logging.info("Moonraker power api connected")
+                    self.power_loger_svitch = True
             except:
-                logging.info("Moonraker power api not responding")
+                if self.power_loger_svitch:
+                    logging.info("Moonraker power api not responding")
+                    self.power_loger_svitch = False
 
         try:
             printer = requests.get("http://localhost:7125/api/printer")
@@ -100,17 +125,28 @@ class StatusMonitor:
             bed_given = str(printer_dict['temperature']['bed']['target'])
             self.bed_temp = calulate_pos(self.bed_max, bed_temp, self.bed_min)
             self.bed_given = calulate_pos(self.bed_max, bed_given, self.bed_min)
-
+            if not self.printer_loger_svitch:
+                logging.info("Moonraker printer api connected")
+                self.printer_loger_svitch = True
         except:
-            logging.info("Moonraker printer api not responding")
+            if self.printer_loger_svitch:
+                logging.info("Moonraker printer api not responding")
+                self.printer_loger_svitch = False
 
         try:
             job = requests.get("http://localhost:7125/api/job")
             job_dict = job.json()
-            self.status = str(job_dict['state'])
-
+            status = str(job_dict['state'])
+            if self.status != status:
+                logging.info("Printer status changed from {} to {}".format(self.status, status))
+                self.status = status
+            if not self.job_loger_svitch:
+                logging.info("Moonraker job api connected")
+                self.job_loger_svitch = True
         except:
-            logging.info("Moonraker job api not responding")
+            if self.job_loger_svitch:
+                logging.info("Moonraker job api not responding")
+                self.job_loger_svitch = False
 
         try:
             job_progerss = requests.get("http://localhost:7125/printer/objects/query?virtual_sdcard=progress")
@@ -120,26 +156,36 @@ class StatusMonitor:
                 self.progress = 16
             else:
                 self.progress = float(progress) * 16
-
+            if not self.object_loger_svitch:
+                logging.info("Moonraker printer object connected")
+                self.object_loger_svitch = True
         except:
-            logging.info("Moonraker printer object not responding")
+            if self.object_loger_svitch:
+                logging.info("Moonraker printer object not responding")
+                self.object_loger_svitch = False
 
-        if self.extruder_temp == 0 and not self.t.is_alive():
-                self.t.start()
-                logging.info("Starting animation")
+        if self.extruder_temp == 0 and not self.start_animation.is_alive():
+            logging.info("Starting animation")
+            self.start_animation.start()
 
     def update_pixels(self):
 
         if self.extruder_temp != 0:
-            if self.t.is_alive():
-                self.t.terminate()
+            if self.start_animation.is_alive():
                 logging.info("Stopping animation")
+                self.start_animation.terminate()
 
             progress_color = self.status_to_color_dict[self.status]
             if self.power_monitor and self.power_status == "off":
-                for i in range(48):
-                    self.pixels[i - 1] = (0, 0, 0)
+                if self.led_loger_svitch:
+                    for i in range(48):
+                        self.pixels[i - 1] = (0, 0, 0)
+                    logging.info("Turning down led lights")
+                    self.led_loger_svitch = False
             else:
+                if not self.led_loger_svitch:
+                    logging.info("Displaing status of data")
+                    self.led_loger_svitch = True
                 for i in range(16):
                     if self.bed_temp <= self.bed_given:
                         if i <= self.bed_temp:
@@ -194,7 +240,6 @@ class StatusMonitor:
                     else:
                         self.pixels[((23 - i + self.offsets[self.ring_order[2]]) % 16) + (self.ring_order[2] * 16)] = (
                             0, 0, 0)
-            # print (self.pixels)
             self.pixels.show()
 
 
